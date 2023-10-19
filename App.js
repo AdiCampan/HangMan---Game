c86180f802aa45b53cec6e5a1297488aa767965e
Adi Campan <adicampan1974@gmail.com>
2023-10-19 19:58:54 +0200

    add sound package

--- App.js
+++ App.js
@@ -15,16 +15,28 @@ import gameOver from "./assets/gameOver.png";
 import ConfettiCannon from "react-native-confetti-cannon";
 import winImage from "./assets/winner.png";
 import { words8 } from "./words8";
+import audioWin from "./assets/audioWin.mp3";
+// import Sound from "react-native-sound";
+import { Audio } from "expo-av";
 import { useEffect, useState } from "react";
 
 export default function App() {
+  const [sound, setSound] = useState();
   const [alphabetButtons, setAlphabetButtons] = useState();
   const [imageIndex, setImageIndex] = useState(0);
   const [image, setImage] = useState(null);
   const [word, setWord] = useState();
   const [secretWord, setSecretWord] = useState();
+  const [tip, setTip] = useState();
   const [winner, setWinner] = useState(false);
   const randomWord = words8[Math.floor(Math.random() * words8.length)];
+
+  async function play() {
+    await Audio.Sound.createAsync(require("./assets/audioWin.mp3"), {
+      shouldPlay: true,
+    });
+  }
+
   useEffect(() => {
     setWinner(false);
     setImage(hangMan[1]);
@@ -33,16 +45,21 @@ export default function App() {
     setImageIndex(0);
     setWinner(false);
     setWord(randomWord);
+    setTip();
   };
 
   const gameOverFunction = () => {
+    setAlphabetButtons();
     setImage(lose);
+    setTip();
   };
   useEffect(() => {
     const winnerFind =
       secretWord?.length > 0 &&
       secretWord.every((letter) => letter.visible === true);
     if (winnerFind) {
+      play();
+      setAlphabetButtons();
       setImage(null);
       setWinner(true);
     }
@@ -74,6 +91,9 @@ export default function App() {
     }
   };
 
+  const showtip = () => {
+    setTip(word.tip);
+  };
   const searchLetter = (letter) => {
     const filteredWord = secretWord.map((object) => {
       if (object.value === letter.value) {
@@ -113,7 +133,7 @@ export default function App() {
     );
     console.log("winner", winner);
     setImageIndex(0);
-    const filteredLeters = alphabetButtons.map((object) => {
+    const filteredLeters = alphabetButtons?.map((object) => {
       if (object.visible === false) {
         return { ...object, visible: true };
       } else {
@@ -124,19 +144,25 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <SafeAreaView>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>HANG MAN - GAME</Text>
         </View>
       </SafeAreaView>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity onPress={reset} style={styles.newGameButton}>
-          <Text style={styles.newGameText}> RESET </Text>
-        </TouchableOpacity>
         <TouchableOpacity onPress={newGame} style={styles.newGameButton}>
           <Text style={styles.newGameText}>New Game</Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={reset} style={styles.newGameButton}>
+          <Text style={styles.newGameText}> RESET </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tipContainer}>
+        <TouchableOpacity onPress={showtip} style={styles.tipButton}>
+          <Text style={styles.newGameText}> EASY </Text>
+        </TouchableOpacity>
+        <Text style={styles.tip}> {tip}</Text>
       </View>
       <View style={styles.image}>
         {/* {winner && (
@@ -146,7 +172,12 @@ export default function App() {
           />
         )} */}
         <Image
-          style={{ width: "80%", height: "100%", resizeMode: "contain" }}
+          style={{
+            width: "70%",
+            height: "100%",
+            resizeMode: "contain",
+            marginLeft: 50,
+          }}
           source={!winner ? image : winImage}
         />
       </View>
@@ -184,12 +215,13 @@ export default function App() {
       </View>
       {winner && <ConfettiCannon count={200} origin={{ x: -10, y: 0 }} />}
       {/* <StatusBar style="auto" /> */}
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 5,
     flex: 1,
     backgroundColor: "lightcyan",
     alignItems: "center",
@@ -201,9 +233,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   titleContainer: {
+    width: 400,
+    // width: "100%",
     padding: 4,
     alignItems: "center",
-    backgroundColor: "grey",
+    backgroundColor: "lightsalmon",
   },
   buttonsContainer: {
     display: "flex",
@@ -211,15 +245,37 @@ const styles = StyleSheet.create({
   },
   newGameButton: {
     margin: 5,
-    height: 50,
-    width: 110,
+    height: 40,
+    width: 100,
     backgroundColor: "#007bff", // Color de fondo del botón (azul en este caso)
-    padding: 15, // Espaciado interior
+    padding: 10, // Espaciado interior
     borderRadius: 15, // Borde redondeado para dar relieve
     elevation: 3, // Nivel de sombra (para Android)
     shadowColor: "rgba(0, 0, 0, 0.2)", // Color de la sombra (para iOS)
     shadowOffset: { width: 0, height: 2 }, // Desplazamiento de la sombra (para iOS)
     shadowOpacity: 1, // Opacidad de la sombra (para iOS)
+  },
+  tipContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  tip: {
+    fontSize: 25,
+  },
+  tipButton: {
+    margin: 5,
+    height: 40,
+    width: 100,
+    backgroundColor: "lawngreen", // Color de fondo del botón (azul en este caso)
+    padding: 10, // Espaciado interior
+    borderRadius: 15, // Borde redondeado para dar relieve
+    elevation: 3, // Nivel de sombra (para Android)
+    shadowColor: "rgba(0, 0, 0, 0.2)", // Color de la sombra (para iOS)
+    shadowOffset: { width: 0, height: 2 }, // Desplazamiento de la sombra (para iOS)
+    shadowOpacity: 1,
   },
   newGameText: {
     color: "white", // Color del texto
@@ -271,6 +327,7 @@ const styles = StyleSheet.create({
     borderBottomColor: "blue",
   },
   alphabetButtonsContainer: {
+    justifyContent: "center",
     marginTop: 20,
     borderRadius: 10,
     width: 300,
